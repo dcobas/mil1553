f00c87261fff1e0571dff5fff90bc05111cc1a61
Julian Lewis <julian.lewis@cern.ch>
2011-04-05 10:26:00 +0200

    librti: This library handles RTI modules by calling the libmil1553 API
    This library handles the RTI memory buffers and status and control functions.
    
    Signed-off-by: Julian Lewis <julian.lewis@cern.ch>

---
+++ lib/librti.c
@@ -0,0 +1,457 @@
+#include <libmil1553.h>
+#include <librti.h>
+#include <unistd.h>
+
+#define DEBUG
+
+/* ===================================== */
+
+#define TYPEDESC_LEN 30
+struct rti_type_s {
+
+	char typedesc[TYPEDESC_LEN];
+	unsigned short sigl;
+	unsigned short sigh;
+};
+
+#define RTI_TYPES 9
+
+struct rti_type_s rti_types[RTI_TYPES] = {
+
+	{ "CMIG Interface G64 3h",     0xFFFF, 0xFFFF },
+	{ "CMIH Interface G64 6h",     0xFFFF, 0xFFFF },
+	{ "CMIM Interface MICENE",     0xFFFF, 0xFFFF },
+	{ "CMIG PS G64 Interface",     0xFFFD, 0xFFFD },
+	{ "CMMX MPX Timing",           0xFFA0, 0xFFA7 },
+	{ "CMMT MPX Timing",           0xFFA8, 0xFFAF },
+	{ "CMCG Command Response G64", 0xFFD0, 0xFFDF },
+	{ "CMIO RelayBox REBOOT",      0xFFCF, 0xFFCF },
+	{ "CMIO RelayBox MILO16",      0xFFC0, 0xFFCE }
+};
+
+
+char *rtilib_sig_to_str(unsigned short sig) {
+
+	int i;
+	struct rti_type_s *rti_type;
+
+	for (i=0; i<RTI_TYPES; i++) {
+		rti_type = &rti_types[i];
+		if ((sig >= rti_type->sigl) && (sig <= rti_type->sigh))
+			return rti_type->typedesc;
+	}
+	return "RTI? Unknown RTI signature";
+}
+
+/* ===================================== */
+
+char *csr_names[16] = { "TB:",  "RB:",  "INV:", "RL:",
+			"INE:", "INT:", "RTP:", "RRP:",
+			"BC:",  "BCR:", "LRR:", "NEM:",
+			"BRD:", "LOC:", "TES:", "OPE:" };
+
+char *rtilib_csr_to_str(unsigned short csr) {
+
+	int i;
+	static char res[128];
+
+	bzero((void *) res, 128);
+
+	for (i=0; i<16; i++) {
+		if ((1<<i) & csr)
+		   strcat(res,csr_names[i]);
+	}
+	return res;
+}
+
+/* ===================================== */
+
+char *str_names[STR_RTI_SHIFT] = { "TF:",  "DBC:", "SF:",  "BUY:",
+				   "BRO:", "TB:",  "RB:",  "TIM:",
+				   "SR:",  "INS:", "ME:" };
+
+char *rtilib_str_to_str(unsigned short str) {
+
+	unsigned int i, rti;
+	static char res[128];
+
+	bzero((void *) res, 128);
+
+	rti = (str & STR_RTI_MASK) >> STR_RTI_SHIFT;
+	sprintf(res,"RTI:%02d ",rti);
+
+	for (i=0; i<STR_RTI_SHIFT; i++) {
+		if ((1<<i) & str)
+		   strcat(res,str_names[i]);
+	}
+	return res;
+}
+
+/* ===================================== */
+
+int rtilib_send_receive(int fn,
+			int bc,
+			int rti,
+			int wc,
+			int sa,
+			int tr,
+			int nreply,
+			unsigned short *rxbuf,
+			unsigned short *txbuf) {
+
+	struct mil1553_tx_item_s tx_item;
+	struct mil1553_send_s send;
+	struct mil1553_recv_s recv;
+	unsigned int txreg;
+	int i, cc;
+
+	milib_encode_txreg(&txreg,wc,sa,tr,rti);
+
+	send.item_count = 1;
+	send.tx_item_array = &tx_item;
+
+	tx_item.bc = bc;
+	tx_item.rti_number = rti;
+	tx_item.txreg = txreg;
+	tx_item.no_reply = nreply;
+
+	if (txbuf) {
+		for (i=0; i<wc; i++) {
+			if (i >= TX_BUF_SIZE)
+				break;
+			tx_item.txbuf[i] = txbuf[i];
+		}
+	}
+
+	cc = milib_send(fn, &send);
+	if (cc)
+		return cc;
+
+	if (nreply == REPLY) {
+
+		bzero((void *) &recv, sizeof(struct mil1553_recv_s));
+
+		recv.pk_type = TX_ALL;
+		recv.timeout = 100;
+
+		cc = milib_recv(fn, &recv);
+		if (cc < 0)
+			return cc;
+
+		if (rxbuf) {
+			for (i=0; i<recv.interrupt.wc + 1; i++) {
+				if (i >= RX_BUF_SIZE)
+					break;
+				rxbuf[i] = recv.interrupt.rxbuf[i];
+			}
+		}
+	}
+	return 0;
+}
+
+/* ===================================== */
+
+int rtilib_read_csr(int fn, int bc, int rti, unsigned short *csr, unsigned short *str) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = 1; sa = SA_CSR; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	*str = rxbuf[0];
+	*csr = rxbuf[1];
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_clear_csr(int fn, int bc, int rti, unsigned short csr) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = 1; sa = SA_CLEAR_CSR; tr = TR_WRITE;
+	txbuf[0] = csr;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_set_csr(int fn, int bc, int rti, unsigned short csr) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = 1; sa = SA_SET_CSR; tr = TR_WRITE;
+	txbuf[0] = csr;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+/**
+ * This is just for debugging the RTI
+ */
+
+int rtilib_read_rxbuf(int fn, int bc, int rti, int wc, unsigned short *rxbuf) {
+
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, sa, tr;
+
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	rtilib_clear_csr(fn,bc,rti,CSR_RRP);
+
+	sa = SA_RXBUF; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+/**
+ * The receive buffer RXBUF is what the RTI receives from the BC.
+ */
+
+int rtilib_write_rxbuf(int fn, int bc, int rti, int wc, unsigned short *txbuf) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	int cc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+
+	cc = rtilib_set_csr(fn,bc,rti,CSR_RRP);
+	if (cc)
+		return cc;
+
+	sa = SA_RXBUF; tr = TR_WRITE;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+/**
+ * The receive buffer RXBUF is what the RTI receives from the BC.
+ * Writing to the RTI RXBUF means SEND data to the RTI equipment from DSC.
+ * Check the RB (RXBUF BUSY) bit in the STR (STATUS), error exit if busy.
+ * Reset the RXBUF read/write pointer.
+ * Write the data to the RXBUF.
+ * Set the RB bit (data ready), enable interrupt to end user equipment and do interrupt.
+ *
+ * Note the RB bit is used to synchronise access by the BC and the end user equipment.
+ * The BC sets it to one after writing data, this tells the
+ * RTI data is available, the RTI then reads the data and sets the RB bit
+ * after the data has been written.
+ */
+
+int rtilib_send_eqp(int fn, int bc, int rti, int wc, unsigned short *txbuf) {
+
+	unsigned short str;
+	int cc;
+
+	cc = rtilib_read_str(fn,bc,rti,&str);
+	if (cc)
+		return cc;
+
+	if (STR_RB & str)
+		return -EBUSY;  /* So the equipment hasn't finished the last transaction */
+
+	cc = rtilib_write_rxbuf(fn,bc,rti,wc,txbuf);
+	if (cc)
+		return cc;
+
+	return rtilib_set_csr(fn,bc,rti,CSR_RB | CSR_INT | CSR_INE);
+}
+
+/* ===================================== */
+
+/**
+ * The transmit buffer TXBUF is what the RTI sends to the BC.
+ */
+
+int rtilib_read_txbuf(int fn, int bc, int rti, int wc, unsigned short *rxbuf) {
+
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, sa, tr;
+
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	cc = rtilib_set_csr(fn,bc,rti,CSR_RTP);
+	if (cc)
+		return cc;
+
+	sa = SA_TXBUF; tr = TR_READ;
+	return rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+}
+
+/* ===================================== */
+
+/**
+ * The transmit buffer TXBUF is what the RTI sends to the BC.
+ * Reading from the RTI TX buf means get data from the RTI equipment for the DSC.
+ * Poll the TB (TXBUF READY) bit in the STR (STATUS), exit on timeout.
+ * Reset the TXBUF read/write pointer.
+ * Read the data from the TXBUF.
+ * Clear the TB bit (data read), clear interrupt to end user equipment.
+ *
+ * Note the TB bit is used to synchronise access by the BC and the end user equipment.
+ * The RTI sets it to one after writing data, this tells the BC
+ * data is available, the BC then reads the data and clears th TB bit
+ * after data has been read.
+ */
+
+int rtilib_recv_eqp(int fn, int bc, int rti, int wc, unsigned short *rxbuf) {
+
+	unsigned short str, tb;
+	int i, cc;
+
+	tb = 0;
+	for (i=0; i<10; i++) {
+		cc = rtilib_read_str(fn,bc,rti,&str);
+		if (cc)
+			return cc;
+		tb = str & STR_TB;
+		if (tb) break;
+		usleep(10000);
+	}
+
+	if (!tb)
+		return -ETIMEDOUT;
+
+	cc = rtilib_read_txbuf(fn,bc,rti,wc,rxbuf);
+	if (cc)
+		return cc;
+
+	return rtilib_clear_csr(fn,bc,rti,CSR_TB | CSR_INT);
+}
+
+/* ===================================== */
+
+/**
+ * This is just for debugging the RTI
+ */
+
+int rtilib_write_txbuf(int fn, int bc, int rti, int wc, unsigned short *txbuf) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	int cc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+
+	rtilib_clear_csr(fn,bc,rti,CSR_RTP);
+
+	sa = SA_TXBUF; tr = TR_WRITE;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_read_signature(int fn, int bc, int rti, unsigned short *sig) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = 1; sa = SA_SIGNATURE; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	*sig = rxbuf[1];
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_read_str(int fn, int bc, int rti, unsigned short *str) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = MODE_READ_STR; sa = SA_MODE; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	*str = rxbuf[0];
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_read_last_str(int fn, int bc, int rti, unsigned short *str) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = MODE_READ_LAST_STR; sa = SA_MODE; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	*str = rxbuf[0];
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_master_reset(int fn, int bc, int rti) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = MODE_MASTER_RESET; sa = SA_MODE; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,1,rxbuf,txbuf);
+	return cc;
+}
+
+/* ===================================== */
+
+int rtilib_read_last_cmd(int fn, int bc, int rti, unsigned short *cmd) {
+
+	unsigned short rxbuf[RX_BUF_SIZE];
+	unsigned short txbuf[TX_BUF_SIZE];
+	int cc, wc, sa, tr;
+
+	bzero((void *) rxbuf, sizeof(unsigned short) * RX_BUF_SIZE);
+	bzero((void *) txbuf, sizeof(unsigned short) * TX_BUF_SIZE);
+
+	wc = MODE_READ_LAST_CMD; sa = SA_MODE; tr = TR_READ;
+	cc = rtilib_send_receive(fn,bc,rti,wc,sa,tr,REPLY,rxbuf,txbuf);
+	*cmd = rxbuf[0];
+	return cc;
+}
+
+/* ===================================== */
+
+void rtilib_print_error(int cc) {
+
+	char *cp;
+	if (cc) {
+		if (cc < 0) cc = -cc;
+		cp = strerror(cc);
+		fprintf(stderr,"RTI Error:%s\n",cp);
+	}
+}
---
+++ lib/librti.h
@@ -0,0 +1,96 @@
+#ifndef LIBRTI
+#define LIBRTI
+
+#include <stdio.h>
+#include <errno.h>
+
+#define CSR_TB  0x0001
+#define CSR_RB  0x0002
+#define CSR_INV 0x0004
+#define CSR_RL  0x0008
+#define CSR_INE 0x0010
+#define CSR_INT 0x0020
+#define CSR_RTP 0x0040
+#define CSR_RRP 0x0080
+#define CSR_BC  0x0100
+#define CSR_BCR 0x0200
+#define CSR_LRR 0x0400
+#define CSR_NEM 0x0800
+#define CSR_BRD 0x1000
+#define CSR_LOC 0x2000
+#define CSR_TES 0x4000
+#define CSR_OPE 0x8000
+
+
+#define STR_TF  0x0001
+#define STR_DBC 0x0002
+#define STR_SF  0x0004
+#define STR_BUY 0x0008
+#define STR_BRO 0x0010
+#define STR_TB  0x0020
+#define STR_RB  0x0040
+#define STR_TIM 0x0080
+#define STR_SR  0x0100
+#define STR_INS 0x0200
+#define STR_ME  0x0400
+
+#define STR_RTI_MASK 0xF800
+#define STR_RTI_SHIFT 11
+
+#define STR_RT0 0x0800
+#define STR_RT1 0x1000
+#define STR_RT2 0x2000
+#define STR_RT3 0x4000
+#define STR_RT4 0x8000
+
+#define SA_CSR 1
+#define SA_CLEAR_CSR 7
+#define SA_SET_CSR 1
+#define SA_RXBUF 2
+#define SA_TXBUF 3
+#define SA_SIGNATURE 30
+
+#define SA_MODE 31
+#define MODE_READ_STR 1
+#define MODE_READ_LAST_STR 2
+#define MODE_MASTER_RESET 8
+#define MODE_READ_LAST_CMD 18
+
+#define TR_READ 1
+#define TR_WRITE 0
+
+char *rtilib_csr_to_str(unsigned short csr);
+char *rtilib_str_to_str(unsigned short str);
+char *rtilib_sig_to_str(unsigned short sig);
+
+#define NO_REPLY 1
+#define REPLY 0
+
+int rtilib_send_receive(int fn,
+			int bc,
+			int rti,
+			int wc,
+			int sa,
+			int tr,
+			int nreply,
+			unsigned short *rxbuf,
+			unsigned short *txbuf);
+
+
+int rtilib_read_csr(int fn, int bc, int rti, unsigned short *csr, unsigned short *str);
+int rtilib_clear_csr(int fn, int bc, int rti, unsigned short csr);
+int rtilib_set_csr(int fn, int bc, int rti, unsigned short csr);
+int rtilib_read_rxbuf(int fn, int bc, int rti, int wc, unsigned short *rxbuf);
+int rtilib_write_rxbuf(int fn, int bc, int rti, int wc, unsigned short *txbuf);
+int rtilib_read_txbuf(int fn, int bc, int rti, int wc, unsigned short *rxbuf);
+int rtilib_write_txbuf(int fn, int bc, int rti, int wc, unsigned short *txbuf);
+int rtilib_read_signature(int fn, int bc, int rti, unsigned short *sig);
+int rtilib_read_str(int fn, int bc, int rti, unsigned short *str);
+int rtilib_read_last_str(int fn, int bc, int rti, unsigned short *str);
+int rtilib_master_reset(int fn, int bc, int rti);
+int rtilib_read_last_cmd(int fn, int bc, int rti, unsigned short *cmd);
+int rtilib_send_eqp(int fn, int bc, int rti, int wc, unsigned short *txbuf);
+int rtilib_recv_eqp(int fn, int bc, int rti, int wc, unsigned short *rxbuf);
+void rtilib_print_error(int cc);
+
+#endif
