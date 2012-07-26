a9bf2d92bcf329f8a5426252bb26420186ea3b0d
Juan David Gonzalez Cobas <dcobas@cern.ch>
2012-07-26 16:16:55 +0200

    debug: add user space tools to decode debugs blobs

---
+++ driver/decode.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_DEVS  16
+
+struct checkpoint {
+	int	busy_timeout;
+	int	int_pending_on_busy;
+	int	hstat_busy;
+	int	int_pending;
+	int	int_raised_and_pending;
+};
+
+int main(int argc, char *argv[])
+{
+	char fname[200];
+	
+	FILE *f;
+	int bc = strtoul(argv[1], NULL, 0);
+	struct checkpoint cp[32];
+	int rti;
+
+	snprintf(fname, sizeof(fname), "/sys/kernel/debug/cbmia/checkpoints%d", bc);
+	f = fopen(fname, "rb");
+	fread(cp, sizeof(cp), 1, f);
+	printf("sizeof(cp) = %d\n", sizeof(cp));
+
+	printf("bc:%d    rti %6s %6s %6s %6s %6s\n", bc, "busy", "bintp", "hstb", "intp", "iintp");
+	for (rti = 0; rti < 32; rti++) {
+		printf("bc:%d rti:%02d %6d %6d %6d %6d %6d\n",
+			bc, rti,
+			cp[rti].busy_timeout,
+			cp[rti].int_pending_on_busy,
+			cp[rti].hstat_busy,
+			cp[rti].int_pending,
+			cp[rti].int_raised_and_pending);
+	}
+	return 0;
+}
---
+++ driver/tdecode.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_DEVS  16
+
+struct checkpoint {
+	int	busy_timeout;
+	int	int_pending_on_busy;
+	int	hstat_busy;
+	int	int_pending;
+	int	int_raised_and_pending;
+};
+
+struct tspoint {
+	int		rti;
+	uint64_t	start_tx;
+	uint64_t	write_tx;
+	uint64_t	int_tx;
+	uint64_t	end_tx;
+};
+
+unsigned int sec(uint64_t nsec)
+{
+	return nsec / 1000000000;
+}
+
+unsigned int usec(uint64_t nsec)
+{
+	return (nsec/1000) % 1000000;
+}
+
+int main(int argc, char *argv[])
+{
+	char fname[200];
+	
+	FILE *f;
+	int bc = strtoul(argv[1], NULL, 0);
+	static struct tspoint tp[20000];
+	int i;
+
+	snprintf(fname, sizeof(fname), "/sys/kernel/debug/cbmia/tspoints%d", bc);
+	f = fopen(fname, "rb");
+	fread(tp, sizeof(struct tspoint), 20000, f);
+	printf("sizeof(tp) = %d\n", sizeof(tp));
+
+	printf("bc:%d    rti %18s %18s %18s %18s\n", bc, "strtx", "wrttx", "inttx", "endtx");
+	for (i = 0; i < 20000; i++) {
+		printf("bc:%u rti:%02u %10u.%06u %6u %6u %6u\n",
+			bc, tp[i].rti,
+			sec(tp[i].start_tx), usec(tp[i].start_tx),
+			usec(tp[i].write_tx - tp[i].start_tx),
+			usec(tp[i].int_tx - tp[i].write_tx),
+			usec(tp[i].end_tx - tp[i].int_tx));
+	}
+	return 0;
+}
